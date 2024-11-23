d238094308c9c2ca1296655e66ca8623f48707e2
renat <aslanovrenat7@gmail.com>
2024-11-23 09:46:06 +0300

    Add search method

--- Trie.java
+++ Trie.java
@@ -1,8 +1,40 @@
-public class Trie<T>  {
-    class Node<T> {
+public class Trie {
+
+    TrieNode root;
+
+    class TrieNode {
         //33 or 26
-        Node[] Children = new Node[26];
-        boolean isTerminal;
-        T value;
+        TrieNode[] children = new TrieNode[26];
+        boolean isWord;
+        char value;
     }
+
+    public Trie() {
+        this.root = new TrieNode();
+    }
+
+    public boolean search(String word){
+        TrieNode[] current = root.children;
+        for(int i = 0; i < word.length(); i++){
+            int charNumber = word.charAt(i) - 'a';
+            if(!charInBranch(word.charAt(i), current)){
+                return false;
+            }
+            current = current[charNumber].children;
+        }
+        return true;
+    }
+
+    private boolean charInBranch(Character letter, TrieNode[] current) {
+        return (current[(int) (letter - 'a')].value == letter);
+    }
+
+    public static void main(String[] args) {
+        int a =  'a';
+        int powerAlphabet = 'a' - 'z';
+        System.out.println(a);
+        System.out.println(powerAlphabet);
+
+    }
+
 }
